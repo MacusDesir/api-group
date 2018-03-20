aa348cca383dbf1c8fae9663a2c007a45f8894a4
Macus Desir <macusdesir@gmail.com>
2018-03-20 08:59:06 -0400

    almost got it working again

--- app/controllers/discoverfy_controller.rb
+++ app/controllers/discoverfy_controller.rb
@@ -1,4 +1,5 @@
 class DiscoverfyController < ApplicationController
   def index
+  	artists = RSpotify::Artist.search('Arctic Monkeys')
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ require "action_view/railtie"
 require "action_cable/engine"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
+require 'rspotify'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -27,5 +28,12 @@ module Api
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.before_configuration do
+  	env_file = File.join(Rails.root, 'config', 'local_env.yml')
+  	YAML.load(File.open(env_file)).each do |key, value|
+   	 ENV[key.to_s] = value
+  	end if File.exists?(env_file)
+	end
   end
 end
