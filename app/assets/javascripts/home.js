ad0168207d42984e5f808e19bc873d51918a811d
Gabe <gwomble27@gmail.com>
2018-03-20 15:16:42 -0400

    Modal 80% functioning

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -7,9 +7,6 @@
         //initialize all modals
         $('.modal').modal();
 
-        //now you can open modal from code
-        $('#modal1').modal('open');
-
         //or by click on trigger
         $('.trigger-modal').modal();
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :about
+  # resources :about
   get 'about/index'
 
   get 'discoverfy/index'
